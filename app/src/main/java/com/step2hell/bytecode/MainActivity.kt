568748e3f5ab3a0a14a6cc1865102bf2d978a402
ᴮᵒᵇ <step2hell@qq.com>
2021-11-22 14:20:25 +0800

    chore: first blood

---
+++ app/src/main/java/com/step2hell/bytecode/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.step2hell.bytecode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.hello).setOnClickListener {
+            println("hello world")
+        }
+    }
+}
